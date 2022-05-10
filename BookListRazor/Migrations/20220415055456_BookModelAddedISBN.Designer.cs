f5fed260a8c6fab40f669903140427a2282bb2e4
fringe225 <turalka@code.edu.az>
2022-05-10 19:05:49 +0400

    Done

--- BookListRazor/Migrations/20220415055456_BookModelAddedISBN.Designer.cs
+++ BookListRazor/Migrations/20220415055456_BookModelAddedISBN.Designer.cs
@@ -3,14 +3,16 @@ using BookListRazor.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookListRazor.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220415055456_BookModelAddedISBN")]
+    partial class BookModelAddedISBN
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
